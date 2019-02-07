6b86ef26334396b68700f7838264e5db555bad69
CharlieBodman <charliebodmangithub@gmail.com>
2019-02-06 20:57:38 -0800

    Updated core game module

--- scripts/game.js
+++ scripts/game.js
@@ -1,45 +1,53 @@
-
-//Scenes
 import Boot from './boot';
 import Preload from './preload';
 import GameTitle from './gametitle';
 import Main from './main';
 import GameOver from './gameover';
 import GameConfig from './config';
-
-// Libs
 import PhaserFloodFill from './libs/PhaserFloodFill';
 import CanvasToBlob from './libs/CanvasToBlob';
 
-let game = false;
+/**
+ * Colouring Book Game
+ */
+class Game {
 
-export default {
+    /**
+     * Initialize the game
+     * @param {HtmlElement} containerElement - container to inject game canvas into
+     * @param {object} config - game config
+     */
+    init(containerElement, config) {
 
-    init:(containerElement, config) => {
+        this.destroy();
 
-        if(game === false) {
+        PhaserFloodFill(Phaser);
+        CanvasToBlob(window);
 
-            PhaserFloodFill(Phaser);
-            CanvasToBlob(window);
-            
-            //Set Game Config
-            GameConfig.setConfig(config);
+        //Set Game Config
+        GameConfig.setConfig(config);
 
-            //Start Game
-            game = new Phaser.Game(800, 600, Phaser.CANVAS, containerElement, false, false, true);
-            game.state.add("Boot", Boot);
-            game.state.add("Preload", Preload);
-            game.state.add("GameTitle", GameTitle);
-            game.state.add("Main", Main);
-            game.state.add("GameOver", GameOver);
-            game.state.start("Boot");
+        //Start Game
+        const game = new Phaser.Game(800, 600, Phaser.CANVAS, containerElement, false, false, true);
+        game.state.add("Boot", Boot);
+        game.state.add("Preload", Preload);
+        game.state.add("GameTitle", GameTitle);
+        game.state.add("Main", Main);
+        game.state.add("GameOver", GameOver);
+        game.state.start("Boot");
+
+        this.game = game;
+    }
+
+    /**
+     * Destroys game instance and frees up memory
+     */
+    destroy() {
+        if (this.game) {
+            this.game.destroy();
+            this.game = null;
         }
-    },
-
-    destroy:() => {
-
-        game.destroy();
-        game = false;
-
     }
 }
+
+export default new Game();
